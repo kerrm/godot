c1d1e982a90d03839fc0bb6b74ff0fa283f4be12
Matthew Kerr <matthew.kerr@nrl.navy.mil>
2023-07-18 14:54:12 -0400

    Add some missing functionality and more tests.

--- gti.py
+++ gti.py
@@ -27,6 +27,7 @@ class _Gti():
         a = np.argsort(stops)
         self._t0 = np.atleast_1d(starts)[a]
         self._t1 = np.atleast_1d(stops)[a]
+        assert(np.all(self._t0[1:] >= self._t1[:-1]))
 
     def accept(self,t):
         idx = np.minimum(np.searchsorted(self._t1,t),len(self._t1)-1)
@@ -78,40 +79,61 @@ class _Gti():
     def computeOntime(self):
         return np.sum(self._t1-self._t0)
 
-#def test_Gti():
-t0 = [0.0,0.6,1.5,4.5,5.0,7.1,8.3]
-t1 = [0.4,1.0,2.0,4.7,6.0,7.9,8.5]
-ot0 = [-1.0,0.3,2.5,3.8,5.5,7.0,7.3,7.6,8.4]
-ot1 = [-0.5,0.8,3.5,4.8,5.6,7.2,7.4,8.0,8.6]
-answer_t0 = [0.3,0.6,4.5,5.5,7.1,7.3,7.6,8.4]
-answer_t1 = [0.4,0.8,4.7,5.6,7.2,7.4,7.9,8.5]
-g1 = Gti(t0,t1)
-g2 = Gti(ot0,ot1)
-assert(np.allclose(g1.computeOntime(),3.5))
-g1.intersection(g2)
-assert(np.all(g1._t0==np.asarray(answer_t0)))
-assert(np.all(g1._t1==np.asarray(answer_t1)))
-g2.intersection(g1)
-assert(np.all(g2._t0==np.asarray(answer_t0)))
-assert(np.all(g2._t1==np.asarray(answer_t1)))
-assert(np.allclose(g1.computeOntime(),1.2))
-assert(np.allclose(g2.computeOntime(),1.2))
-g1 = Gti(t0,t1)
-test_accept = [-0.5,0.2,0.5,0.6,1.0,1.3,10]
-accept_answer = np.asarray([0,1,0,1,0,0,0],dtype=bool)
-assert(np.all(g1.accept(test_accept)==accept_answer))
-g3 = g1.applyTimeRangeCut(0,10)
-assert(g1.equal(g3))
-g3 = g1.applyTimeRangeCut(0.2,8.0)
-answer_t0 = [0.2, 0.6, 1.5, 4.5, 5. , 7.1]
-answer_t1 = [0.4, 1. , 2. , 4.7, 6. , 7.9]
-assert(np.all(g3._t0==np.asarray(answer_t0)))
-assert(np.all(g3._t1==np.asarray(answer_t1)))
-assert(np.allclose(g3.computeOntime(),3.1))
+    def minValue(self):
+        """ Return smallest time in GTI.
 
-from skymaps import Gti as cGti
-cg1 = cGti(t0,t1)
-cg2 = cGti(ot0,ot1)
-cg1.intersection(cg2)
-assert(np.allclose(g2._t0,cg1.get_edges(True)))
-assert(np.allclose(g2._t1,cg1.get_edges(False)))
+        By construction, this is the first element the start times.
+        """
+        return self._t0[0]
+
+    def maxValue(self):
+        """ Return largest time in GTI.
+
+        By construction, this is the last element the stop times.
+        """
+        return self._t1[-1]
+
+    def get_edges(self,starts=True):
+        if starts:
+            return self._t0
+        return self._t1
+
+def test_Gti():
+    t0 = [0.0,0.6,1.5,4.5,5.0,7.1,8.3]
+    t1 = [0.4,1.0,2.0,4.7,6.0,7.9,8.5]
+    ot0 = [-1.0,0.3,2.5,3.8,5.5,7.0,7.3,7.6,8.4]
+    ot1 = [-0.5,0.8,3.5,4.8,5.6,7.2,7.4,8.0,8.6]
+    answer_t0 = [0.3,0.6,4.5,5.5,7.1,7.3,7.6,8.4]
+    answer_t1 = [0.4,0.8,4.7,5.6,7.2,7.4,7.9,8.5]
+    g1 = Gti(t0,t1)
+    g2 = Gti(ot0,ot1)
+    assert(np.allclose(g1.computeOntime(),3.5))
+    g1.intersection(g2)
+    assert(np.all(g1._t0==np.asarray(answer_t0)))
+    assert(np.all(g1._t1==np.asarray(answer_t1)))
+    g2.intersection(g1)
+    assert(np.all(g2._t0==np.asarray(answer_t0)))
+    assert(np.all(g2._t1==np.asarray(answer_t1)))
+    assert(np.allclose(g1.computeOntime(),1.2))
+    assert(np.allclose(g2.computeOntime(),1.2))
+    g1 = Gti(t0,t1)
+    test_accept = [-0.5,0.2,0.5,0.6,1.0,1.3,10]
+    accept_answer = np.asarray([0,1,0,1,0,0,0],dtype=bool)
+    assert(np.all(g1.accept(test_accept)==accept_answer))
+    g3 = g1.applyTimeRangeCut(0,10)
+    assert(g1.equal(g3))
+    g3 = g1.applyTimeRangeCut(0.2,8.0)
+    answer_t0 = [0.2, 0.6, 1.5, 4.5, 5. , 7.1]
+    answer_t1 = [0.4, 1. , 2. , 4.7, 6. , 7.9]
+    assert(np.all(g3._t0==np.asarray(answer_t0)))
+    assert(np.all(g3._t1==np.asarray(answer_t1)))
+    assert(np.allclose(g3.computeOntime(),3.1))
+
+    from skymaps import Gti as cGti
+    cg1 = cGti(t0,t1)
+    cg2 = cGti(ot0,ot1)
+    cg1.intersection(cg2)
+    assert(np.allclose(g2._t0,cg1.get_edges(True)))
+    assert(np.allclose(g2.get_edges(True),cg1.get_edges(True)))
+    assert(np.allclose(g2._t1,cg1.get_edges(False)))
+    assert(np.allclose(g2.get_edges(False),cg1.get_edges(False)))
