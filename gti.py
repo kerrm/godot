638c837f859424d73e4307c5443db51f62ea2e4c
Matthew Kerr <matthew.kerr@nrl.navy.mil>
2025-02-10 14:45:34 -0500

    Add implementation for nontrivial GTI combine.

--- gti.py
+++ gti.py
@@ -1,4 +1,3 @@
-# TODO -- finish implementing combine and add a test
 from collections import deque
 
 from astropy.io import fits
@@ -35,17 +34,48 @@ class _Gti():
         return (self._t0[idx] <= t) & (self._t1[idx] > t)
 
     def combine(self,other):
-        if other._t1[-1] < self._t0[0]:
+        if other._t1[-1] <= self._t0[0]:
             # other is entirely before us
             self._t0 = np.append(other._t0,self._t0)
             self._t1 = np.append(other._t1,self._t1)
-        elif other._t0[0] > self._t1[-1]:
+        elif other._t0[0] >= self._t1[-1]:
             # other is entirely after us
             self._t0 = np.append(self._t0,other._t0)
             self._t1 = np.append(self._t1,other._t1)
         else:
-            # there is a non-trivial intersection
-            raise NotImplementedError
+            # there is a non-trivial intersection; complete in two steps,
+            # truncating any intersecting GTIs such that they don't overlap,
+            # then merging any GTIs with identical boundaries
+            st0 = np.append(self._t0,np.inf)
+            ot0 = np.append(other._t0,np.inf)
+            st1 = np.append(self._t1,np.inf)
+            ot1 = np.append(other._t1,np.inf)
+            i0 = np.searchsorted(ot1,self._t1)
+            i1 = np.searchsorted(st1,other._t1)
+            # TODO -- need to handle the case of the far edge, I think
+            new_st1 = np.minimum(self._t1,ot0[i0])
+            new_ot1 = np.minimum(st0[i1],other._t1)
+            new_t0 = np.append(self._t0,other._t0)
+            new_t1 = np.append(new_st1,new_ot1)
+            dt = new_t1-new_t0
+            new_t0 = new_t0[dt>0]
+            new_t1 = new_t1[dt>0]
+            a = np.argsort(new_t1)
+            new_t0 = new_t0[a]
+            new_t1 = new_t1[a]
+            nt0 = [new_t0[0]]
+            nt1 = []
+            for i in range(1,len(new_t0)-1):
+                if new_t0[i] == new_t1[i-1]:
+                    continue
+                else:
+                    nt1.append(new_t1[i-1])
+                    nt0.append(new_t0[i])
+            nt1.append(new_t1[-1])
+            assert(len(nt1)==len(nt0))
+            self._t0 = np.asarray(nt0)
+            self._t1 = np.asarray(nt1)
+        return self
 
     def intersection(self,other):
         """ Defined via SWIG in the old days to get &=.
@@ -112,22 +142,26 @@ class _Gti():
             return self._t0
         return self._t1
 
-def test_Gti():
+def test_Gti(get_test_data=False):
     t0 = [0.0,0.6,1.5,4.5,5.0,7.1,8.3]
     t1 = [0.4,1.0,2.0,4.7,6.0,7.9,8.5]
     ot0 = [-1.0,0.3,2.5,3.8,5.5,7.0,7.3,7.6,8.4]
     ot1 = [-0.5,0.8,3.5,4.8,5.6,7.2,7.4,8.0,8.6]
-    answer_t0 = [0.3,0.6,4.5,5.5,7.1,7.3,7.6,8.4]
-    answer_t1 = [0.4,0.8,4.7,5.6,7.2,7.4,7.9,8.5]
+    intersect_answer_t0 = np.asarray([0.3,0.6,4.5,5.5,7.1,7.3,7.6,8.4])
+    intersect_answer_t1 = np.asarray([0.4,0.8,4.7,5.6,7.2,7.4,7.9,8.5])
+    union_answer_t0 = np.asarray([-1.0,0.0,1.5,2.5,3.8,5.0,7.0,8.3])
+    union_answer_t1 = np.asarray([-0.5,1.0,2.0,3.5,4.8,6.0,8.0,8.6])
+    if get_test_data:
+        return t0,t1,ot0,ot1,intersect_answer_t0,intersect_answer_t1,union_answer_t0,union_answer_t1
     g1 = Gti(t0,t1)
     g2 = Gti(ot0,ot1)
     assert(np.allclose(g1.computeOntime(),3.5))
     g1.intersection(g2)
-    assert(np.all(g1._t0==np.asarray(answer_t0)))
-    assert(np.all(g1._t1==np.asarray(answer_t1)))
+    assert(np.all(g1._t0==intersect_answer_t0))
+    assert(np.all(g1._t1==intersect_answer_t1))
     g2.intersection(g1)
-    assert(np.all(g2._t0==np.asarray(answer_t0)))
-    assert(np.all(g2._t1==np.asarray(answer_t1)))
+    assert(np.all(g2._t0==intersect_answer_t0))
+    assert(np.all(g2._t1==intersect_answer_t1))
     assert(np.allclose(g1.computeOntime(),1.2))
     assert(np.allclose(g2.computeOntime(),1.2))
     g1 = Gti(t0,t1)
@@ -137,18 +171,27 @@ def test_Gti():
     g3 = g1.applyTimeRangeCut(0,10)
     assert(g1.equal(g3))
     g3 = g1.applyTimeRangeCut(0.2,8.0)
-    answer_t0 = [0.2, 0.6, 1.5, 4.5, 5. , 7.1]
-    answer_t1 = [0.4, 1. , 2. , 4.7, 6. , 7.9]
-    assert(np.all(g3._t0==np.asarray(answer_t0)))
-    assert(np.all(g3._t1==np.asarray(answer_t1)))
+    answer_t0 = np.asarray([0.2, 0.6, 1.5, 4.5, 5. , 7.1])
+    answer_t1 = np.asarray([0.4, 1. , 2. , 4.7, 6. , 7.9])
+    assert(np.all(g3._t0==answer_t0))
+    assert(np.all(g3._t1==answer_t1))
     assert(np.allclose(g3.computeOntime(),3.1))
 
-    from skymaps import Gti as cGti
-    cg1 = cGti(t0,t1)
-    cg2 = cGti(ot0,ot1)
-    cg1.intersection(cg2)
-    assert(np.allclose(g2._t0,cg1.get_edges(True)))
-    assert(np.allclose(g2.get_edges(True),cg1.get_edges(True)))
-    assert(np.allclose(g2._t1,cg1.get_edges(False)))
-    assert(np.allclose(g2.get_edges(False),cg1.get_edges(False)))
+    g1 = Gti(t0,t1)
+    g2 = Gti(ot0,ot1)
+    g1.combine(g2)
+    assert(np.all(g1._t0==union_answer_t0))
+    assert(np.all(g1._t1==union_answer_t1))
 
+
+    try:
+        from skymaps import Gti as cGti
+        cg1 = cGti(t0,t1)
+        cg2 = cGti(ot0,ot1)
+        cg1.intersection(cg2)
+        assert(np.allclose(g2._t0,cg1.get_edges(True)))
+        assert(np.allclose(g2.get_edges(True),cg1.get_edges(True)))
+        assert(np.allclose(g2._t1,cg1.get_edges(False)))
+        assert(np.allclose(g2.get_edges(False),cg1.get_edges(False)))
+    except ImportError:
+        print('Could not complete comparison against C++ implementation.')
